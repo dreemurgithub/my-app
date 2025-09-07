32b4aad18977b54e9bcd589cf02d87f0b9ff12f5
Dat, Le Thanh <lethanhdat1993@gmail.com>
2025-09-07 22:48:36 +0700

    sum

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,41 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const sumOne = (n: number) => {
+    if (n < 0) return 0
+    // brute force
+    let i = 0;
+    let sum = 0;
+    for (i; i <= n; i++) sum += i;
+    return sum;
+  };
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  const sumTwo = (n: number) => {
+    if (n < 0) return 0
+    // brute force, from higher to less
+    let i = n;
+    let sum = 0
+    while (i > 0) {
+      sum += i
+      i--
+    }
+    return sum
+  };
+
+  const sumMath = (n: number) => {
+    if (n < 0) return 0
+    // just basic math
+    // 2 * Sum == (1 + n) + (2 + n-1) +.... (n + 1) // iterate from 1 to n
+    return (n * (n + 1)) / 2; // n is the total pair, n+1 is the value of each pair
+  };
+  const [n, setN] = useState(0)
+  return <div>
+    <input type="number" min={0} value={n} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+      setN(Number(e.target.value))
+    }} />
+    <p>sum one {sumOne(n)}</p>
+    <p>sum two {sumTwo(n)}</p>
+    <p>sum math {sumMath(n)}</p>
+  </div>;
 }
